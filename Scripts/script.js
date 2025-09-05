68b828614f070c016d135eceb2ed98a664e47b3c
HasinIshrakK <hasinishrakk@gmail.com>
2025-09-05 21:57:04 +0600

    Congratulating modal added

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,110 +1,98 @@
 let r = 1;
 let c = 1;
 
-inputKey = document.getElementById("input");
-inputKey.addEventListener("keyup", (event) => {
-    if (event.key.toLowerCase() === "a") {
-        if (c > 1) {
-            c = c - 1;
+function goal() {
+    const f = document.querySelectorAll(".box");
+    f.forEach(element => {
+        const t = [...element.classList];
+        if (t.includes("star") && t.includes("selector") == true) {
+            document.getElementById("my_modal_5").showModal();
         }
-        else {
-            c = 5;
-        }
-    }
-    else if (event.key.toLowerCase() === "d") {
-        if (c < 5) {
-            c = c + 1;
-        }
-        else {
-            c = 1;
-        }
+    });
+}
 
-    }
-    else if (event.key.toLowerCase() === "w") {
-        if (r > 1) {
-            r = r - 1;
-        }
-        else {
-            r = 5;
-        }
-
-    }
-    else if (event.key.toLowerCase() === "s") {
-        if (r < 5) {
-            r = r + 1;
-        }
-        else {
-            r = 1;
-        }
-
-    }
+function select() {
     const d = document.querySelectorAll(".box");
     d.forEach((f) => {
         f.classList.remove("selector");
     })
-    selector = document.getElementById(`r${r}-c${c}`);
+    let selector = document.getElementById(`r${r}-c${c}`);
 
     selector.classList.add("selector");
 }
-)
 
-document.getElementById("up").addEventListener('click', () => {
+function up() {
     if (r > 1) {
         r = r - 1;
     }
     else {
         r = 5;
     }
-    const d = document.querySelectorAll(".box");
-    d.forEach((f) => {
-        f.classList.remove("selector");
-    })
-    selector = document.getElementById(`r${r}-c${c}`);
+}
 
-    selector.classList.add("selector");
-})
-document.getElementById("left").addEventListener('click', () => {
+function left() {
     if (c > 1) {
         c = c - 1;
     }
     else {
         c = 5;
     }
-    const d = document.querySelectorAll(".box");
-    d.forEach((f) => {
-        f.classList.remove("selector");
-    })
-    selector = document.getElementById(`r${r}-c${c}`);
+}
 
-    selector.classList.add("selector");
-})
-document.getElementById("right").addEventListener('click', () => {
+function right() {
     if (c < 5) {
         c = c + 1;
     }
     else {
         c = 1;
     }
-    const d = document.querySelectorAll(".box");
-    d.forEach((f) => {
-        f.classList.remove("selector");
-    })
-    selector = document.getElementById(`r${r}-c${c}`);
+}
 
-    selector.classList.add("selector");
-})
-document.getElementById("down").addEventListener('click', () => {
+function down() {
     if (r < 5) {
         r = r + 1;
     }
     else {
         r = 1;
     }
-    const d = document.querySelectorAll(".box");
-    d.forEach((f) => {
-        f.classList.remove("selector");
-    })
-    selector = document.getElementById(`r${r}-c${c}`);
+}
 
-    selector.classList.add("selector");
+inputKey = document.getElementById("input");
+inputKey.addEventListener("keyup", (event) => {
+    if (event.key.toLowerCase() === "a") {
+        left();
+    }
+    else if (event.key.toLowerCase() === "d") {
+        right();
+    }
+    else if (event.key.toLowerCase() === "w") {
+        up();
+    }
+    else if (event.key.toLowerCase() === "s") {
+        down();
+    }
+    select();
+    goal();
+}
+)
+
+document.getElementById("up").addEventListener('click', () => {
+    up();
+    select();
+    goal();
+})
+document.getElementById("left").addEventListener('click', () => {
+    left();
+    select();
+    goal();
+})
+document.getElementById("right").addEventListener('click', () => {
+    right();
+    select();
+    goal();
+})
+document.getElementById("down").addEventListener('click', () => {
+    down();
+    select();
+    goal();
 })
